6cdaccfcbc8ab923bab93db3f32d6ea6489ebcd9
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-11-04 01:07:14 +0800

    modify: routing and views redirecting links testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  get 'landing/index'
+  namespace :admins do
+    # get 'admin/home',  to: 'home#index'
+    root 'home#index'
+  end
+  namespace :clients do
+    # get 'client/home',  to: 'home#index'
+    root 'home#index'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
