909c58c6c98105b0f8543453e8a7742d1e74e742
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-11-04 02:58:12 +0800

    modify: linking pages, sign out button for both admin and client and modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,35 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   namespace :admins do
-    # get 'admin/home',  to: 'home#index'
+    devise_for :users, controllers: { sessions: 'admins/sessions' }, skip: [:registrations]
     root 'home#index'
   end
+
   namespace :clients do
-    # get 'client/home',  to: 'home#index'
+    devise_for :users, controllers: { registrations: 'clients/registrations', sessions: 'clients/sessions' }
     root 'home#index'
   end
+
+  # devise_for :users
+  #
+  # namespace :admins do
+  #   root 'home#index'
+  #   devise_for :users, controllers: {
+  #     sessions: 'admins/sessions'
+  #   }
+  # end
+  #
+  # namespace :clients do
+  #   root 'home#index'
+  #   devise_for :users, controllers: {
+  #     registrations: 'clients/registrations',
+  #     sessions: 'clients/sessions'
+  #   }
+  # end
+  #
+  # authenticated :user do
+  #   root 'clients/home#index', as: :authenticated_root
+  # end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
