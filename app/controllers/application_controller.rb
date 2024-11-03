382c87223620638670485c895333552910218c52
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-11-04 02:55:32 +0800

    modify: application controller for sign in path that handle the session logic for clients and admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protected
+
+  def after_sign_in_path_for(resource)
+    if resource.admin?
+      admins_root_path
+    else
+      clients_root_path
+    end
+  end
 end
