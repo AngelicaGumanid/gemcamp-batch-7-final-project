61c01dfa5d845160c6a72955daafbdca8ae4a833
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-11-14 05:57:57 +0800

    modify: routes root to redirection

--- config/routes.rb
+++ config/routes.rb
@@ -59,5 +59,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "clients/home#index"
+  root to: redirect('http://client.com:3000/clients/')
 end
