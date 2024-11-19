48fbd5fb3bd7ef172c49262aaba900ccbab439f6
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-11-19 18:55:57 +0800

    add: [Feature 12] controller and model for category

---
+++ app/controllers/admins/categories_controller.rb
@@ -0,0 +1,65 @@
+class Admins::CategoriesController < ApplicationController
+  before_action :set_category, only: %i[show edit update destroy restore]
+
+  def index
+    @categories = Category.with_deleted
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to admins_categories_path, notice: 'Category successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to admins_categories_path, notice: 'Category successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @category.destroy
+      redirect_to admins_categories_path, notice: 'Category successfully soft deleted.'
+    else
+      redirect_to admins_categories_path, alert: @category.errors.full_messages.to_sentence
+    end
+  end
+
+  def restore
+    if @category
+      @category.update(deleted_at: nil)
+      redirect_to admins_categories_path, notice: 'Category successfully restored.'
+    else
+      redirect_to admins_categories_path, alert: 'Category not found.'
+    end
+  end
+
+  private
+
+  def set_category
+    @category = Category.with_deleted.find_by(id: params[:id])
+    unless @category
+      redirect_to admins_categories_path, alert: 'Category not found.'
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+end
---
+++ app/models/category.rb
@@ -0,0 +1,23 @@
+class Category < ApplicationRecord
+  has_many :items
+
+  default_scope { where(deleted_at: nil) }
+  scope :with_deleted, -> { unscope(where: :deleted_at) }
+
+  validates :name, presence: true, uniqueness: true
+
+  before_destroy :ensure_no_items
+
+  def destroy
+    update(deleted_at: Time.current)
+  end
+
+  private
+
+  def ensure_no_items
+    if items.exists?
+      errors.add(:base, "Cannot delete category with associated items")
+      throw(:abort)
+    end
+  end
+end
