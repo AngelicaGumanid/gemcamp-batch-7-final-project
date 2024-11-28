b8651caf79f08544953d14a58aa4a5702af14a26
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-11-29 01:30:10 +0800

    modify: [Feature 16] modified model and routes needed for the ticket buying

--- app/controllers/clients/lotteries_controller.rb
+++ app/controllers/clients/lotteries_controller.rb
@@ -1,7 +1,8 @@
 class Clients::LotteriesController < ApplicationController
   layout 'client'
 
-  skip_before_action :authenticate_user!, only: :index
+  before_action :authenticate_clients_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :buy_ticket]
 
   def index
     @categories = Category.includes(:items)
@@ -10,11 +11,52 @@ class Clients::LotteriesController < ApplicationController
 
     if params[:category].present?
       @items = items_scope.where(category_id: params[:category])
-      Rails.logger.debug "Filtering by category: #{params[:category]}"
+      # Rails.logger.debug "Filtering by category: #{params[:category]}"
     else
       @items = items_scope
-      Rails.logger.debug "No category filter applied"
+      # Rails.logger.debug "No category filter applied"
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+
+    redirect_to root_path, alert: "Item is not available for purchase" unless @item.state == "starting"
+
+    @user_tickets = @item.tickets.where(user: current_clients_user)
+    @ticket_count = @user_tickets.count
+  end
+
+  def buy_ticket
+    ticket_count = params[:ticket_count].to_i
+
+    if ticket_count <= 0
+      redirect_to clients_lottery_path(@item), alert: "Invalid ticket count."
+      return
+    end
+
+    if current_clients_user.coins >= ticket_count
+      current_clients_user.decrement!(:coins, ticket_count)
+
+      Ticket.transaction do
+        ticket_count.times do |i|
+          ticket = current_clients_user.tickets.create(item: @item, batch_count: @item.batch_count)
+          unless ticket.persisted?
+            raise ActiveRecord::Rollback, "Ticket #{i + 1} failed: #{ticket.errors.full_messages.join(', ')}"
+          end
+        end
+      end
+
+      redirect_to clients_lottery_path(@item), notice: "#{ticket_count} ticket(s) purchased successfully."
+    else
+      redirect_to clients_lottery_path(@item), alert: "You do not have enough coins."
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -26,24 +26,18 @@ class Ticket < ApplicationRecord
   end
 
   def generate_serial_number
-    # Get the current date in yymmdd format
     date_part = Time.current.strftime("%y%m%d")
 
-    # Get the associated item (assuming the ticket belongs to an item)
     item = self.item
 
-    # Ensure batch_count is set. If nil, default to 1 or another value.
     batch_count = item.batch_count || 1
 
-    # Count the number of tickets for the current item and batch (starting from 0001)
     number_count = Ticket.where(item_id: item.id, batch_count: batch_count).count + 1
-    number_count = number_count.to_s.rjust(4, '0') # Ensure 4 digits, pad with leading zeros
+    number_count = number_count.to_s.rjust(4, '0')
 
-    # Generate the serial number in the desired format
     serial_number = "#{date_part}-#{item.id}-#{batch_count}-#{number_count}"
 
-    # Return the generated serial number
-    serial_number
+    self.serial_number = serial_number
   end
 
   private
