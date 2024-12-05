6649b2e3313618270283cbc9ad55a80cfe17df3d
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-12-05 15:31:36 +0800

    modify: viewing and functionality of invite list for the client profile page

--- app/controllers/clients/profiles/invite_list_controller.rb
+++ app/controllers/clients/profiles/invite_list_controller.rb
@@ -2,6 +2,6 @@ class Clients::Profiles::InviteListController < ApplicationController
   layout 'client'
 
   def index
-    @invites = User.where(parent: current_clients_user).page(params[:page]).per(5)
+    @invites = User.where(parent_id: current_clients_user.id).order(created_at: :desc).page(params[:page]).per(5)
   end
 end
