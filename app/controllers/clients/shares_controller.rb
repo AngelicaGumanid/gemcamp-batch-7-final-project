b7ac1d178a08be40ac087131d0c93ad18d32f8ae
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-12-10 07:44:45 +0800

    modify: share view and controller for published state callingi in the vie page

--- app/controllers/clients/shares_controller.rb
+++ app/controllers/clients/shares_controller.rb
@@ -1,11 +1,15 @@
 class Clients::SharesController < ApplicationController
   layout 'client'
 
+  # Skip authentication to allow public access
+  before_action :authenticate_clients_user!, except: [:index, :show]
+
   def index
-    @shares = Item.where(state: 'published')
+    @shares = Winner.where(state: 'published').includes(:item, :user)
   end
 
   def show
-    @share = Item.find(params[:id])
+    @share = Winner.find_by(id: params[:id], state: 'published')
+    redirect_to clients_shares_path, alert: 'Share not found' unless @share
   end
 end
