3d4f152a50fdbb09b4eb5b88b4fef99bf267573c
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-12-12 11:26:05 +0800

    add: [Feature 31] banner migration to the database

---
+++ db/migrate/20241212031801_add_deleted_at_to_banners.rb
@@ -0,0 +1,5 @@
+class AddDeletedAtToBanners < ActiveRecord::Migration[7.0]
+  def change
+    add_column :banners, :deleted_at, :datetime, default: nil
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_12_024052) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_12_031801) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "city_id"
     t.string "code"
@@ -43,6 +43,16 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_12_024052) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "banners", charset: "utf8mb4", force: :cascade do |t|
+    t.string "preview"
+    t.datetime "online_at"
+    t.datetime "offline_at"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
   end
 
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
