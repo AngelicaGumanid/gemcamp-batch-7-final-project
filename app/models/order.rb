f42f83db4cb92b5e15e3ab21b3286f6f3c318577
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-12-13 20:08:06 +0800

    modify: fixed order model transition and functionality of paid, submit and cancel

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,15 +2,18 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :offer, optional: true
 
-  enum genre: { deposit: 0, increase: 1, deduct: 2, bonus: 3, share: 4 }
-
-  include AASM
-
   validates :serial_number, presence: true, uniqueness: true
+  # validates :remarks, presence: true
   validates :genre, presence: true
+  validate :offer_required_if_deposit, if: :deposit?
+  validate :amount_and_coins_required_if_deposit, if: :deposit?
 
   before_validation :generate_serial_number, on: :create
   after_save :update_user_balance, if: :saved_change_to_state?
+
+  enum genre: { deposit: 0, increase: 1, deduct: 2, bonus: 3, share: 4 }
+
+  include AASM
 
   aasm column: 'state' do
     state :pending, initial: true
@@ -21,8 +24,7 @@ class Order < ApplicationRecord
     end
 
     event :cancel do
-      transitions from: :pending, to: :cancelled, after: :handle_cancel
-      transitions from: :submitted, to: :cancelled, after: :handle_cancel
+      transitions from: [:pending, :submitted, :paid], to: :cancelled, after: :handle_cancel
     end
 
     event :pay do
@@ -37,15 +39,59 @@ class Order < ApplicationRecord
     self.serial_number = "#{date_part}-#{user.id}-#{number_count}"
   end
 
+  def may_submit?
+    state == 'pending'
+
+  end
+
   def can_pay?
     state == 'submitted' && amount > 0
   end
 
   def can_cancel?
-    state == 'pending' || state == 'submitted'
+    state == 'pending' || state == 'submitted' || state == 'paid'
   end
 
   private
+
+  def handle_payment
+    if genre == 'deduct'
+      decrease_user_coins(coin)
+    else
+      increase_user_coins(coin)
+    end
+    increase_total_deposit if genre == 'deposit'
+  end
+
+  def handle_cancel
+    if genre == 'deduct'
+      increase_user_coins(coin)
+    else
+      decrease_user_coins(coin) if user.coins >= coin
+    end
+    decrease_total_deposit if genre == 'deposit'
+  end
+
+  def increase_total_deposit
+    user.increment!(:total_deposit, amount || 0)
+  end
+
+  def decrease_total_deposit
+    user.decrement!(:total_deposit, amount || 0)
+    genre
+  end
+
+  def increase_user_coins(coin)
+    user.increment!(:coins, coin)
+  end
+
+  def decrease_user_coins(coin)
+    if user.coins >= coin
+      user.decrement!(:coins, coin)
+    else
+      raise "Insufficient coins to perform deduction."
+    end
+  end
 
   def update_user_balance
     case genre
@@ -62,24 +108,27 @@ class Order < ApplicationRecord
     end
   end
 
-  def handle_cancel
-    if genre != 'deduct'
-      user.increment!(:coins, coin) if user.coins >= coin
-    else
-      user.decrement!(:coins, coin)
-    end
-    update_total_deposit if genre == 'deposit'
-  end
+  # def update_total_deposit
+  #   if genre == 'deposit'
+  #     user.increment!(:total_deposit, amount || 0)
+  #   end
+  # end
 
-  def handle_payment
-    if genre == 'deposit'
-      update_total_deposit
+  def offer_required_if_deposit
+    if genre == 'deposit' && offer.nil?
+      errors.add(:offer, "must be present for deposit orders.")
     end
   end
 
-  def update_total_deposit
+  def amount_and_coins_required_if_deposit
     if genre == 'deposit'
-      user.increment!(:total_deposit, amount || 0)
+      if amount.nil? || amount <= 0
+        errors.add(:amount, "must be present and greater than 0 for deposit orders.")
+      end
+      if coin.nil? || coin <= 0
+        errors.add(:coin, "must be present and greater than 0 for deposit orders.")
+      end
     end
   end
+
 end
